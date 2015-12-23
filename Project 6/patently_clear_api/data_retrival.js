de3a027ca3a1cb9723dde9e88385d4bdc81549a6
Ravin Kumar <ravinsdrive@gmail.com>
2015-12-22 18:03:19 -0800

    Initial API Calls for Patently Clear Complete

--- Project 6/patently_clear_api/data_retrival.js
+++ Project 6/patently_clear_api/data_retrival.js
@@ -4,17 +4,72 @@
 
 var fs = require('fs');
 var PatentlyClear = require('patentlyclear')
+var jsonfile = require('jsonfile')
 
-var p
 
+var results = []
 var key = fs.readFileSync("apikey.txt").toString();
 console.log(key);
 
 pc = new PatentlyClear(key);
 
-pc.getById('US20140000001', function(err,data) {
-	debugger;
-	p = data;
-	console.log(p)
-	console.log("Finished")
-	});
+var query = {assignee : "National"};
+
+function all_results() {
+    debugger
+    
+    var hits
+    hits = 0
+    do {
+        pc.search(query, function(err, data) {
+          
+          query.scroll_id = data.scroll_id;
+          results = results.concat(data);
+          
+          jsonfile.writeFile("output.json", results, {spaces: 2}, function (err) {
+          console.error(err)
+          });
+                  
+          // Get number of objects from each query
+          hits = data["total"]
+          console.log(hits)
+      });
+    }
+    while(hits > 0);
+  };
+
+/*
+pc.search(query, function(err,data) {
+  query.scroll_id = data.scroll_id;
+  results = results.concat(data);
+  debugger;
+  console.log(data);
+  });
+*/
+
+
+function single_result() {
+   
+  console.log("Single Query Working");
+  
+  pc.search(query, function(err,data) {
+    query.scroll_id = data.scroll_id;
+    debugger
+    results = results.concat(data);
+    })
+  };
+
+
+
+  
+function test_counter() {
+  var i = 0
+  do {
+      text = "The number is " + i;
+      console.log(text);
+      i++;
+  }
+  while (true);
+};
+
+all_results()
